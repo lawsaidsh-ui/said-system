d3150459fa500f99d1d520922c81bf5f0067e7b7
fuadcitybeach-prog <fuadcitybeach@gmail.com>
2026-06-30 19:24:32 +0400

    Fix Alembic revision id length

--- alembic/versions/0012_pay_voucher.py
+++ alembic/versions/0012_pay_voucher.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 
-revision: str = "0012_payment_voucher_client_matter"
+revision: str = "0012_pay_voucher"
 down_revision: Union[str, None] = "0011_whatsapp_manual_link"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
